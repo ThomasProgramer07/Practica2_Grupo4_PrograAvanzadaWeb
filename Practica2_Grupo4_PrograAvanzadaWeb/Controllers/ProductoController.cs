28bac1c047cd222711c481b5b5cc5f3f3a2a82ad
nvalencias <nvalencia30948@ufide.ac.cr>
2026-07-04 09:09:32 -0600

    Create, Read Productos

--- Practica2_Grupo4_PrograAvanzadaWeb/Controllers/ProductoController.cs
+++ Practica2_Grupo4_PrograAvanzadaWeb/Controllers/ProductoController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Practica2_Grupo4_PrograAvanzadaWeb.BLL.Dtos;
+using Practica2_Grupo4_PrograAvanzadaWeb.BLL.Services.Categoria;
 using Practica2_Grupo4_PrograAvanzadaWeb.BLL.Services.Producto;
 
 namespace Practica2_Grupo4_PrograAvanzadaWeb.Controllers;
@@ -6,16 +9,41 @@ namespace Practica2_Grupo4_PrograAvanzadaWeb.Controllers;
 public class ProductoController : Controller
 {
     private readonly IProductoServicio _productoServicio;
+    private readonly ICategoriaServicio _categoriaServicio; 
 
-    public ProductoController(IProductoServicio productoServicio)
+    public ProductoController(IProductoServicio productoServicio, ICategoriaServicio categoriaServicio)
     {
         _productoServicio = productoServicio;
+        _categoriaServicio = categoriaServicio;
     }
 
     public async Task<IActionResult> Index()
     {
         var respuesta = await _productoServicio.GetProductos();
-        return View(respuesta.Datos);
+        var respuestaCategorias = await _categoriaServicio.GetCategorias();
+        ViewBag.Categorias = new SelectList(respuestaCategorias.Datos ?? new List<CategoriaDto>(), "Id", "Nombre");
+
+        return View(respuesta.Datos ?? new List<ProductoDto>());
     }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateProducto(ProductoDto producto)
+    {
+        if (!ModelState.IsValid)
+        {
+            var respuestaCategorias = await _categoriaServicio.GetCategorias();
+            ViewBag.Categorias = new SelectList(respuestaCategorias.Datos ?? new List<CategoriaDto>(), "Id", "Nombre");
+            return BadRequest(ModelState);
+        }
+
+        var respuesta = await _productoServicio.CreateProducto(producto);
+        if (!respuesta.Exito)
+        {
+            TempData["Error"] = respuesta.Mensaje;
+            return RedirectToAction(nameof(Index));
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
 }
